68f390f81baa36fd137ec815a7e747da709a0a01
coolesting <coolesting@gmail.com>
2014-11-05 00:49:12 -0800

    update the templates for new appearance of F 5

--- view/views/assets/main.js
+++ view/views/assets/main.js
@@ -1,6 +1,16 @@
 
 $(document).foundation();
 
-$('body').click(function(){
-	console.log('hello world');
+//$('body').click(function(){
+//	console.log('hello world');
+//});
+
+// form effect
+
+$('.form .submit a').click(function(){
+	$(this).each(function(){
+		var val = $(this).attr('value');
+		$(this).parents('.submit').find('.submit-value').val(val);
+		$(this).parents('.form').submit();
+	});
 });
